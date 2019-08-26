1706be25ac9db79b180368360bc76d301ae79197
sangwansangwan <45001621+sangwansangwan@users.noreply.github.com>
2019-08-27 00:02:01 +0530

    Add files via upload
    updated

--- img.py
+++ img.py
@@ -2,56 +2,73 @@ import time
 import img2pdf 
 from PIL import Image 
 import os 
+
+
+
+
+
 start = time.time()
 
-pdf_path = '/home/ubuntu111000/Desktop/Combined.pdf'
-#cwd=raw_input('Enter the dir:')
-cwd='/home/ubuntu111000/project'
-basedir=cwd
 
+basedir='/home/ubuntu111000/project'
+temp=os.listdir(basedir)
+temp.sort()
+print temp
+
+print " "
+
+
+namevar=raw_input("Name for output file: ")
+pdf_path = '/home/ubuntu111000/'+namevar+'.pdf'
 
 ######################################################################################################
 
 
 def main(): 
-	i=1
+	i=input("Starting Number::")
 	var=os.listdir(basedir)
 	var.sort()
-	print var
+	
 	
 	for filename in var:
-		
+
 		src=basedir+'/'+filename
 		dst=basedir+'/'+str(i)+'.jpg'
 		os.rename(src,dst)
 		i=i+1
+	
+		
+		
 		print src,"-->",dst,"\n"
-		 
+				 
 
 #######################################################################################################
 
 def main2():
 	var1=os.listdir(basedir)
 	var1.sort()
-	myarr=[]
-	print var1
 	
+	
+	myarr=[]
+	
+		
 	for filees in var1:
-  
-    		varr=cwd+'/'+filees
+  			
+    		varr=basedir+'/'+filees
      		myarr.append(varr)
 
 	pdf_bytes = img2pdf.convert(myarr) 
 	file = open(pdf_path, "wb") 
 	file.write(pdf_bytes) 
 	file.close()
-	     	
+	
 ########################################################################################################
  
     	
 
 main()
-main2()
+#main2()
+
 end=time.time()
 print('DATA built IN ',end-start,"seconds\n")
 print('No erros!!!') 
