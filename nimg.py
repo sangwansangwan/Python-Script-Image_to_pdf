f55bcde961818cc789ddc86eb1126bec748f6cca
sangwansangwan <45001621+sangwansangwan@users.noreply.github.com>
2019-09-05 00:31:07 +0530

    Add files via upload
    Working

--- nimg.py
+++ nimg.py
@@ -11,27 +11,37 @@ start = time.time()
 
 
 basedir='/home/ubuntu111000/project'
-temp=os.listdir(basedir)
-temp.sort()
-print temp
 
 print " "
 
 
-namevar=raw_input("Name for output file: ")
-pdf_path = '/home/ubuntu111000/'+namevar+'.pdf'
+ 
+
+bake=raw_input("Enter 'PDF' if you want to make pdf or 'REN' for just renaming: ")
+
+if bake == 'PDF':
+	namevar=raw_input("Name for output file: ")
+	pdf_path = '/home/ubuntu111000/'+namevar+'.pdf'
 
 ######################################################################################################
 
 
-def main(): 
-	i=input("Starting Number::")
+def main():
+	
+	if bake=='PDF':
+		i=1
+	else:
+		i=input("Enter The starting digit: ")
+	
 	var=os.listdir(basedir)
-	var.sort()
+	var.sort(key=lambda f: int(filter(str.isdigit, f)))
+	
 	
 	
 	for filename in var:
 
+		
+		
 		src=basedir+'/'+filename
 		dst=basedir+'/'+str(i)+'.jpg'
 		os.rename(src,dst)
@@ -40,14 +50,14 @@ def main():
 		
 		
 		print src,"-->",dst,"\n"
-				 
+			 
 
 #######################################################################################################
 
 def main2():
 	var1=os.listdir(basedir)
-	var1.sort()
-	
+	var1.sort(key=lambda f: int(filter(str.isdigit, f)))
+	print var1	
 	
 	myarr=[]
 	
@@ -62,12 +72,14 @@ def main2():
 	file.write(pdf_bytes) 
 	file.close()
 	
+
 ########################################################################################################
  
     	
 
 main()
-#main2()
+if bake=='PDF':
+	main2()
 
 end=time.time()
 print('DATA built IN ',end-start,"seconds\n")
